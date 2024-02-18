4d8c49f9362f8f24832197c5865885b6e96042f9
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2024-02-18 09:31:51 -0300

    :lipstick: updated PostBody responsivity

--- components/Post/PostBody.jsx
+++ components/Post/PostBody.jsx
@@ -5,14 +5,14 @@ export default function PostBody ({ content }) {
   return (
     <div className="max-w flex justify-start gap-4">
 
-      <div className="w-3/4 mx-10">
+      <div className="w-full md:w-3/4 mx-10">
         <div
           className={markdownStyles['markdown']}
           dangerouslySetInnerHTML={{ __html: content }}
         />
       </div>
 
-      <div className="w-1/4 h-20">
+      <div className="w-full md:w-1/4 h-20">
         <Adsense />
       </div>
     </div>
