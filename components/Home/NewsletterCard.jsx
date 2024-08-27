7314b0e43b56335aab638a67ccbd673e316a9bc4
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2024-08-27 08:12:15 -0300

    :bug: fixed newsletter route import

--- components/Home/NewsletterCard.jsx
+++ components/Home/NewsletterCard.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { MailIcon } from '../Icons';
+import { useRouter } from 'next/router'
 
 const NewsletterCard = () => {
     const router = useRouter();
